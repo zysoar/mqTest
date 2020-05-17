c92cb3f64729004282537f6215e4cca484f44c8a
zysoar <zysoar@sina.cn>
2020-05-17 20:54:16 +0800

    hi

--- src/main/java/cn/com/jms/activeMQ/Consumer.java
+++ src/main/java/cn/com/jms/activeMQ/Consumer.java
@@ -33,9 +33,9 @@ public class Consumer extends Thread implements MessageListener {
      */  
     public ActiveMQConnectionFactory getConnectionFactory(){  
         // default null  
-        String user=ActiveMQConnection.DEFAULT_USER;  
+        String usermay=ActiveMQConnection.DEFAULT_USER;
         // default null  
-        String password=ActiveMQConnection.DEFAULT_PASSWORD;  
+        String password=ActiveMQConnection.DEFAULT_PASSWORD;
         // default failover://tcp://localhost:61616  
         String url=ActiveMQConnection.DEFAULT_BROKER_URL;  
         return this.getConnectionFactory(user, password, url);  
